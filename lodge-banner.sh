67c2239466722171431cef1665e8ecd534071c8d
Osiris Ortiz <osirisortizpc@gmail.com>
2025-10-29 16:08:36 -0400

    Add Twin Peaks Red Room banner

--- lodge-banner.sh
+++ lodge-banner.sh
@@ -1,12 +1,32 @@
 #!/data/data/com.termux/files/usr/bin/bash
-echo -e "
-\033[38;5;124m██████████████████████████████████\033[0m
-\033[38;5;160m██████████████████████████████████\033[0m
-\033[38;5;196m██████████\033[0m\033[38;5;88m░░░░░░░░\033[0m\033[38;5;196m██████████\033[0m
-\033[38;5;196m██████████\033[0m\033[38;5;52m░░░░░░░░\033[0m\033[38;5;196m██████████\033[0m
-\033[38;5;52m░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░\033[0m
-\033[38;5;7mVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV\033[0m
-\033[38;5;0m^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\033[0m
+# Twin Peaks Red Room Banner – Agent Cooper Edition
 
-\033[38;5;203mWelcome back to the Red Room, Agent Cooper.\033[0m ☕
+# Color palette
+RED='\033[38;5;160m'
+DARKRED='\033[38;5;52m'
+WHITE='\033[38;5;15m'
+RESET='\033[0m'
+
+# Random Twin Peaks quote
+QUOTES=(
+  "Through the darkness of future past, the magician longs to see."
+  "We live inside a dream."
+  "When you see me again, it won’t be me."
+  "The owls are not what they seem."
+  "Where we're from, the birds sing a pretty song…"
+)
+RANDOM_QUOTE=${QUOTES[$RANDOM % ${#QUOTES[@]}]}
+
+clear
+echo -e "
+${RED}██████████████████████████████████████████${RESET}
+${RED}██████████████████████████████████████████${RESET}
+${DARKRED}██████${RED}░░░░░░░░░░░░░░░░░░░░░░░░${DARKRED}██████${RESET}
+${DARKRED}██████${RED}░░░░░░░░░░░░░░░░░░░░░░░░${DARKRED}██████${RESET}
+${DARKRED}░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░${RESET}
+${WHITE}VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV${RESET}
+${DARKRED}^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^${RESET}
+
+${RED}${RANDOM_QUOTE}${RESET}
+${WHITE}Welcome back to the Red Room, Agent Cooper.${RESET} ☕
 "
